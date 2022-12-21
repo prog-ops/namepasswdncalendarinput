fdf3fd90234e678a32afc90a2536a622a728f6b4
prog-ops <59245989+prog-ops@users.noreply.github.com>
2022-12-21 22:59:09 +0700

    debugging

--- src/components/TheCalendar.tsx
+++ src/components/TheCalendar.tsx
@@ -50,7 +50,7 @@ export default function TheCalendar() {
                     showDaysOutsideCurrentMonth={true}
                     // * Su Mo Tu We Th Fr Sa
                     dayOfWeekFormatter={(day) => `${day}`}
-                    label="Birth day"
+                    label="Birthday"
                     value={value}
                     onChange={handleChange}
                     closeOnSelect={false}
