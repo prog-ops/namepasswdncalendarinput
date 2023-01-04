7ab81e26f187c7c73dccdb9ded9300654befc4b3
prog-ops <nomail12345@gmail.com>
2023-01-04 23:12:48 +0700

    debug month btn not shown on live page

--- src/styles/paperSxProps.ts
+++ src/styles/paperSxProps.ts
@@ -7,21 +7,23 @@ export const PaperSxProps: SxProps = {
     alignItems: "center",
     justifyItems: "center",
     justifyContent: 'space-evenly',
+    position: 'relative'
     // mb: '18px'
   },
-  "& .MuiPickersCalendarHeader-root:first-child": {
-    order: 0,
-    paddingRight: "20px",
-    paddingLeft: "20px"
-  },
+  // "& .MuiPickersCalendarHeader-root:first-child": {
+  //   order: 0,
+  //   paddingRight: "20px",
+  //   paddingLeft: "20px"
+  // },
   "& .MuiPickersArrowSwitcher-root": {
     display: "inline-flex",
+    position: "absolute",
     // mt: '24px'
   },
-  // "& .MuiPickersArrowSwitcher-root:first-child": {
-  //   order: 0,
-  // },
-  // January 2023 between arrows
+  // // "& .MuiPickersArrowSwitcher-root:first-child": {
+  // //   order: 0,
+  // // },
+  // // January 2023 between arrows
   // "& .css-dplwbx-MuiPickersCalendarHeader-label": {
   "& .MuiPickersCalendarHeader-label": {
     textAlign: "center",
@@ -34,27 +36,30 @@ export const PaperSxProps: SxProps = {
   "& .MuiPickersCalendarHeader-labelContainer": {
     order: 1,
     color: colors.common.white,
+      position: "absolute",
   },
   "& .MuiPickersCalendarHeader-switchViewButton": {
     display: 'none',
   },
   "& .MuiPickersArrowSwitcher-spacer": {
-    width: "120px",
-    // width: "220px",
+    width: "220px",
     marginLeft: 0.5,
     marginRight: 0.5
   },
-  "& .css-31ca4x-MuiPickersFadeTransitionGroup-root": {
-    display: "flex",
-    position: "absolute",
-    paddingLeft: "100px",
-    // mb: '24px'
-  },
-  "& .css-9reuh9-MuiPickersArrowSwitcher-root": {
-      marginLeft: "-2px"
-  },
+  // "& .css-31ca4x-MuiPickersFadeTransitionGroup-root": {
+  //   display: "flex",
+  //   position: "absolute",
+  //   // paddingLeft: "100px",
+  //   // mb: '24px'
+  // },
+  // "& .css-9reuh9-MuiPickersArrowSwitcher-root": {
+  //     marginLeft: "-2px"
+  // },
   "& .MuiPickersArrowSwitcher-button": {
-    paddingRight: "7px",
-    color: colors.common.white
+    // paddingRight: "7px",
+    // color: colors.common.white
+  },
+  "& .MuiIconButton-edgeStart": {
+    ml: '-20px'
   },
 }
